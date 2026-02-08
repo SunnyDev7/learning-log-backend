b050f82d059a04d68936d4c7b63890cb8f92aa40
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-08 19:11:54 +0100

    get categories route ready

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { createCategory } from "../controllers/category.controller.js";
+import {
+  createCategory,
+  getCategories,
+} from "../controllers/category.controller.js";
 import { authenticationMiddleware } from "../middlewares/authentication.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", authenticationMiddleware, createCategory);
+router.get("/:id", authenticationMiddleware, getCategories);
 
 export default router;
