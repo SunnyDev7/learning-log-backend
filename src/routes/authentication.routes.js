5c952dfb54fb21028c38532cbb50c6e1cbb7994d
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-08 17:42:41 +0100

    logour route created, need work from client side

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -3,6 +3,7 @@ import express from "express";
 import {
   getLoggedInUser,
   logIn,
+  logOut,
   register,
 } from "../controllers/authentication.controller.js";
 import { authenticationMiddleware } from "../middlewares/authentication.middleware.js";
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", logIn);
 router.get("/user", authenticationMiddleware, getLoggedInUser);
+router.get("/logout", authenticationMiddleware, logOut);
 
 export default router;
