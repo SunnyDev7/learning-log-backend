58e163b53c177d0393e4cac74836468e2b9e69fb
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-11 15:09:01 +0100

    update targer route ready

--- src/controllers/target.controller.js
+++ src/controllers/target.controller.js
@@ -19,3 +19,29 @@ export const getTargets = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateTargets = async (req, res, next) => {
+  try {
+    let targets = await Target.findOne({ userId: req.user.id });
+
+    if (!targets) {
+      targets = await Target.create({
+        userId: req.user.id,
+        ...req.body,
+      });
+    } else {
+      targets = await Target.findOneAndUpdate(
+        { userId: req.user.id },
+        { ...req.body, updatedAt: Date.now() },
+        { new: true, runValidators: true },
+      );
+    }
+
+    res.json({
+      success: true,
+      data: targets,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/target.routes.js
+++ src/routes/target.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 import { authenticationMiddleware } from "../middlewares/authentication.middleware.js";
-import { getTargets } from "../controllers/target.controller.js";
+import { getTargets, updateTargets } from "../controllers/target.controller.js";
 
 const router = express.Router();
 
 router.use(authenticationMiddleware);
 
 router.get("/", getTargets);
+router.put("/update", updateTargets);
 
 export default router;
