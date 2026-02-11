1f44b92307afb325815cc3b9b25314bdee6cb3f4
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-11 14:14:44 +0100

    stats calculator added but not fully functional as of now

--- src/routes/stats.routes.js
+++ src/routes/stats.routes.js
@@ -1,7 +1,12 @@
 import express from "express";
 
+import { authenticationMiddleware } from "../middlewares/authentication.middleware.js";
+import { getDashboardStats } from "../controllers/stats.controller.js";
+
 const router = express.Router();
 
-router.get("/");
+router.use(authenticationMiddleware);
+
+router.get("/dashboard", getDashboardStats);
 
 export default router;
