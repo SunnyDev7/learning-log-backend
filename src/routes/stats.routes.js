5b6ab8e140fad8f57af47359851128d99aca7354
SunnyDev7 <sunnydev4589@gmail.com>
2026-03-15 22:31:00 +0100

    get yearly stats controller route

--- src/routes/stats.routes.js
+++ src/routes/stats.routes.js
@@ -5,6 +5,7 @@ import {
   getDailyStats,
   getDashboardStats,
   getWeeklyStats,
+  getYearlyStats,
 } from "../controllers/stats.controller.js";
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.use(authenticationMiddleware);
 
 router.get("/dashboard", getDashboardStats);
 router.get("/weekly", getWeeklyStats);
+router.get("/yearly", getYearlyStats);
 router.get("/daily/:date", getDailyStats);
 
 export default router;
